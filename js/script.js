bb170099e4e7b7aeddfc92cfaf1b960f4243e5b9
François-Marie d'Aboville <fmdaboville@gmail.com>
2018-03-28 14:40:02 +0200

    add animate scroll to menu and items

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ Authors : 	François-Marie d'Aboville
 			Jehan Poublan
 */
 
+$(document).ready(function(){
+    // au clic sur un lien
+   $('.scroll').on('click', function() { // Au clic sur un élément
+			var page = $(this).attr('href'); // Page cible
+			var speed = 750; // Durée de l'animation (en ms)
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
+});
